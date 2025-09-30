c1c5c59963ede82373302e5637055991aaf1dec1
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-30 20:46:48 +0600

    4th commit

--- json.js
+++ json.js
@@ -1,1 +1,15 @@
-console.log('Explore Api');
+// console.log('Explore Api');
+
+const person ={
+    name:'liam',
+    isStudent:true,
+    friends:['ariful,rabiul,mustaq'],
+    age:22
+
+}
+console.log(person , typeof person);
+
+// JSON.stringfy  json format e convert kora jai
+
+const personjason = JSON.stringify(person);
+console.log(personjason , typeof personjason);
