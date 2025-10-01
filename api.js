47f318b8db290ef278443454b20f98a62e8c298a
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-01 13:25:03 +0600

    10th commit

--- api.js
+++ api.js
@@ -1,35 +1,20 @@
 
 
 
-const loadData=()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-
-      }
-
-
       const loadPost=()=>{
         const url ='https://jsonplaceholder.typicode.com/posts';
         fetch(url)
         .then(response=> response.json())
         .then(json=> displayPost(json))
       }
-      // 1.get the container
+   const displayPost=(posts)=>{
+    // 1.Get The container
+    posts.forEach(posts=>{
+      const postContainer = document.getElementById('post-container');
+      const div = document.createElement('div');
+      // console.log(div)
 
-      const postContainer = document.getElementById('post-container')
+      div.appendChild(postContainer);
 
-
-      const displayPost = (posts)=>{
-        posts.forEach((post)=>{
-            // console.log(post.id);
-            // 2. Create HTML Element
-            const li = document.createElement('li');
-            li.innerText = post.title
-
-            // 3.add li into container
-
-            postContainer.appendChild(li);
-
-        })
-      }
+    })
+   }
