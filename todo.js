f0171c13184155f9b05e967f68c2894a5aafc022
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-01 14:48:10 +0600

    14th commit

--- todo.js
+++ todo.js
@@ -1,3 +1,32 @@
 const loadPost=()=>{
-    console.log('click')
+    const url="https://jsonplaceholder.typicode.com/todos";
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>displayPost(json))
+
 }
+
+const displayPost=(posts)=>{
+   const postContainer=  document.getElementById('post-container');
+   postContainer.innerHTML=" ";
+
+   posts.forEach=(post=>{
+    const div2 = document.createElement('div');
+    div2.innerHTML=`
+    <div class="post-card">
+            <h1>${post.title}</h1>
+            <p>${post.comopleted}</p>
+        </div>
+    
+    
+    `
+
+
+   })
+postContainer.append(div2);
+
+
+
+
+}
+
